5bc506ea81c2bb9217fc689a4dcd7fd2f0aee990
Waga Odongo <wagaodongo@gmail.com>
2019-05-06 21:41:04 +0300

    google Login
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
+import { GoogleLogin } from "react-google-login";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+	const onSuccess = (googleUser) => {
+		const idToken = googleUser.getAuthResponse().id_token;
+
+		console.log({ idToken });
+	};
+
+	return (
+		<GoogleLogin
+			clientId="721762092596-dlbsi4mv2cgm8eh400eevt9v5ggr4ogv.apps.googleusercontent.com"
+			onSuccess={onSuccess}
+			isSignedIn={true}
+		/>
+	);
 };
 
 const styles = {
-  root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
-  }
+	root: {
+		height: "100vh",
+		display: "flex",
+		justifyContent: "center",
+		flexDirection: "column",
+		alignItems: "center"
+	}
 };
 
 export default withStyles(styles)(Login);
