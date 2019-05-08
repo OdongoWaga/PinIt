e94ba18e659f45b9ae42e1fb4b5979e1e7a4b8ce
Waga Odongo <wagaodongo@gmail.com>
2019-05-08 14:49:17 +0300

    login
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -4,7 +4,7 @@ import Typography from "@material-ui/core/Typography";
 import { GoogleLogin } from "react-google-login";
 import { GraphQLClient } from "graphql-request";
 
-const ME_QUERY=`
+const ME_QUERY = `
 {
   me {
     _id
@@ -13,18 +13,18 @@ const ME_QUERY=`
     picture
   }
 }
-`
+`;
 
 const Login = ({ classes }) => {
-	const onSuccess = async(googleUser) => {
-    const idToken = googleUser.getAuthResponse().id_token;
-    
-    const client = new GraphQLClient('http://localhost:4000/graphql'. {
-      headers: {authorization: idToken}
-    })
+	const onSuccess = async (googleUser) => {
+		const idToken = googleUser.getAuthResponse().id_token;
 
-    const data = await client.request()
-    console.log({data})
+		const client = new GraphQLClient("http://localhost:4000/graphql", {
+			headers: { authorization: idToken }
+		});
+
+		const data = await client.request();
+		console.log({ data });
 	};
 
 	return (
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const { findOrCreateUser } = require("./controllers/userController");
 
 mongoose
 	.connect(process.env.MONGO_URI, { useNewUrlParser: true })
@@ -13,18 +14,21 @@ mongoose
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
-	context: ({ req }) => {
+	context: async ({ req }) => {
 		let authToken = null;
+		let currentUser = null;
 
 		try {
 			authToken = req.headers.authorization;
 
 			if (authToken) {
 				//Find or create User
+				currentUser = await findOrCreateUser(authToken);
 			}
 		} catch (err) {
 			console.error(`Unable to authenticate user with token ${authToken}`);
 		}
+		return { currentUser };
 	}
 });
 
