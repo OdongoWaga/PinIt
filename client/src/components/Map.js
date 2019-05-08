a2cf574f3cb8fe370fac1016656a63bf91c02563
Waga Odongo <wagaodongo@gmail.com>
2019-05-08 22:39:56 +0300

    map
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 import ReactMapGL from "react-map-gl";
 
+const viewport = {
+	latitude: 37.7577,
+	longitude: -122.4376,
+	zoom: 13
+};
+
 const Map = ({ classes }) => {
 	return (
 		<div className={classes.root}>
-			<ReactMapGL mapboxApiAccessToken="">A</ReactMapGL>
+			<ReactMapGL
+				width="100vw"
+				height="calc(100vh- 64px)"
+				mapStyle="mapbox://styles/mapbox/streets-v9"
+				mapboxApiAccessToken="pk.eyJ1Ijoid2FnYSIsImEiOiJjanZmbHV0MnEweTh3NGRvNjdhaGZvczgxIn0.SqZ2KjPIeMTnoFQLldb2sg"
+				{...viewport}
+			/>
 		</div>
 	);
 };
